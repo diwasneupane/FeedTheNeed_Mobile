4cec1d74d41267161819064feb57d256f6bea2da
netra-1 <diwas.neupane55@gmail.com>
2022-12-05 15:45:11 +0545

    blog section user created

--- lib/screens/blogscreen.dart
+++ lib/screens/blogscreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:feedtheneed/repositories/blog_repository.dart';
 import 'package:feedtheneed/repositories/partner_reporitory.dart';
 import 'package:feedtheneed/screens/blog_description.dart';
@@ -137,7 +136,155 @@ class _BlogScreenState extends State<BlogScreen> {
                       }
                     },
                   ),
-                 
+                  FutureBuilder<List<dynamic>>(
+                    future: BlogRepository().getBlogs(),
+                    builder: (context, snapshot) {
+                      // debugPrint(snapshot.data.toString());
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (snapshot.data != null) {
+                          List<dynamic> lstblog = snapshot.data!;
+                          // debugPrint("dsfsjnj${lstblog.length}");
+                          return SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.53,
+                            child: ListView.builder(
+                              itemCount: lstblog.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                String? blogid = lstblog[index]["_id"];
+                                return snapshot.data![index]!["blog_name"]!
+                                        .toLowerCase()
+                                        .contains(searchQuery.toLowerCase())
+                                    ? Padding(
+                                        padding:
+                                            const EdgeInsets.only(top: 15.0),
+                                        child: Container(
+                                          height: 95,
+                                          width: double.infinity,
+                                          decoration: BoxDecoration(
+                                            color: const Color.fromARGB(
+                                                255, 255, 255, 255),
+                                            borderRadius:
+                                                BorderRadius.circular(20),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.black
+                                                    .withOpacity(0.06),
+                                                spreadRadius: 0,
+                                                blurRadius: 4,
+                                                offset: const Offset(0,
+                                                    4), // changes position of shadow
+                                              ),
+                                            ],
+                                          ),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              SizedBox(
+                                                height: 90,
+                                                width: 100,
+                                                child: ClipRRect(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          11.0),
+                                                  child: Image.network(
+                                                    "$baseUrl${lstblog[index]["blog_image"]!}",
+                                                    fit: BoxFit.cover,
+                                                  ),
+                                                ),
+                                              ),
+                                              const SizedBox(width: 18),
+                                              Expanded(
+                                                child: Container(
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .center,
+                                                    children: [
+                                                      Text(
+                                                        lstblog[index]
+                                                            ["blog_name"],
+                                                        style: const TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.w500,
+                                                          fontSize: 16.0,
+                                                        ),
+                                                      ),
+                                                      const SizedBox(height: 4),
+                                                      Text(
+                                                        lstblog[index]
+                                                            ["blog_category"],
+                                                      )
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                              const SizedBox(width: 8),
+                                              Row(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  IconButton(
+                                                    onPressed: () {
+                                                      Navigator.of(context)
+                                                          .push(
+                                                        MaterialPageRoute(
+                                                            builder: (context) =>
+                                                                BlogDescription(
+                                                                    blogId:
+                                                                        blogid!)),
+                                                      );
+                                                      //action coe when button is pressed
+                                                    },
+                                                    icon: const Icon(
+                                                      Icons
+                                                          .arrow_forward_ios_outlined,
+                                                      color: Color(0xFF41A2CD),
+                                                      size: 16,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                    : const SizedBox();
+                              },
+                            ),
+                          );
+                        } else {
+                          return const Center(
+                            child: Text("No data"),
+                          );
+                        }
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                Color(0xff754A4A)),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
